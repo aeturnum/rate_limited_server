4bcc7ee6e603f7a480d7e9a4dc3734caf4adebb6
Daniel Drexler <aeturnum@gmail.com>
2021-02-15 16:02:33 -0800

    Change = to match-specific <-

--- lib/web.ex
+++ lib/web.ex
@@ -17,18 +17,13 @@ defmodule RateLimitedServer.Web do
   end
 
   get "/receive-message" do
-    # For some reason the with clause isn't working like I expect so I need to do this dumb try catch block?
-    try do
-      with conn <- Plug.Conn.fetch_query_params(conn),
-           {:ok, queue_name} = Map.fetch(conn.query_params, "queue"),
-           {:ok, message} = Map.fetch(conn.query_params, "message") do
-        Jobs.add_job(queue_name, fn -> IO.puts(message) end)
-        Plug.Conn.send_resp(conn, 200, "")
-      end
-    rescue
-      MatchError ->
-        IO.puts("Got match error in recieve-message")
-        Plug.Conn.send_resp(conn, 400, "Incorrect Parameters")
+    with conn <- Plug.Conn.fetch_query_params(conn),
+         {:ok, queue_name} <- Map.fetch(conn.query_params, "queue"),
+         {:ok, message} <- Map.fetch(conn.query_params, "message") do
+      Jobs.add_job(queue_name, fn -> IO.puts(message) end)
+      Plug.Conn.send_resp(conn, 200, "")
+    else
+      :error -> Plug.Conn.send_resp(conn, 400, "Incorrect Parameters")
     end
   end
 
